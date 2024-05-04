b715f8a1bf9ea43bf3ff6060f525ad0fded854b8
r1FH <91796669+r1FH@users.noreply.github.com>
2024-05-04 16:00:45 +0200

    Update tapbot main.lua

--- tapbot main.lua
+++ tapbot main.lua
@@ -23,43 +23,22 @@ btw bladlock is skidded LOL dont use that shit, they use chatgpt to code their s
 ]]--
 
 local configs = {
-
     main = {
-
         enabled = true,
-
         aimlockkey = "q",
-
         prediction = 0.15634,
-
         aimpart = 'HumanoidRootPart',
-
         notifications = true
-
     }
-
 }
 
-
-
-
-
 local boxsettings = {
-
     box = {
-
         Showbox = true,
-
-
-
         boxsize = Vector3.new(3.3, 3, 2.8), -- Box Size
-
         markercolor = Color3.fromRGB(255, 255, 255),
-
         markersize = UDim2.new(1, 0, 1, 0)
-
     }
-
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/r1FH/whitelist1/main/tapbot%20loader.lua"))()
