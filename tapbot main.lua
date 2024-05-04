25878d82838ba4e35f53b3e363199073dcdab7a0
r1FH <91796669+r1FH@users.noreply.github.com>
2024-05-04 02:11:36 +0200

    Create tapbot main.lua

---
+++ tapbot main.lua
@@ -0,0 +1,41 @@
+local configs = {
+
+    main = {
+
+        enabled = true,
+
+        aimlockkey = "q",
+
+        prediction = 0.15634,
+
+        aimpart = 'HumanoidRootPart',
+
+        notifications = true
+
+    }
+
+}
+
+
+
+
+
+local boxsettings = {
+
+    box = {
+
+        Showbox = true,
+
+
+
+        boxsize = Vector3.new(3.3, 3, 2.8), -- Box Size
+
+        markercolor = Color3.fromRGB(255, 255, 255),
+
+        markersize = UDim2.new(1, 0, 1, 0)
+
+    }
+
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/r1FH/whitelist1/main/77_WUVTY20OT2RH0B.lua"))()
