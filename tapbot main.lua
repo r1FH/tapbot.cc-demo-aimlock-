507a85ddaba0255a26d97f85ff849381579543f1
r1FH <91796669+r1FH@users.noreply.github.com>
2024-05-04 14:49:29 +0200

    Update tapbot main.lua

--- tapbot main.lua
+++ tapbot main.lua
@@ -38,4 +38,4 @@ local boxsettings = {
 
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/r1FH/whitelist1/main/tapbot%20whitelist.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/r1FH/whitelist1/main/77_NN6A1.lua"))()
